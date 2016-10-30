04cb2a1d58851ea5dce0da4b39206d115e17a118
Giang Nguyen <giangnb@live.com>
2016-10-30 15:36:26 +0700

    [CRITICAL] Fix manipilation of entity identity fileds

--- src/java/com/marksmana/entities/Score.java
+++ src/java/com/marksmana/entities/Score.java
@@ -8,6 +8,8 @@ package com.marksmana.entities;
 import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -33,6 +35,7 @@ public class Score implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Basic(optional = false)
     private Long id;
     @Basic(optional = false)
--- src/java/com/marksmana/entities/Student.java
+++ src/java/com/marksmana/entities/Student.java
@@ -10,6 +10,8 @@ import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -37,6 +39,7 @@ public class Student implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Basic(optional = false)
     private Integer id;
     @Basic(optional = false)
